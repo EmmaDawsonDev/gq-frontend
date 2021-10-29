3f946797670d37d229d9176bc127c250e7458e44
Emma Dawson <emma.l.dawson@gmail.com>
2021-10-29 12:12:37 +0200

    rename files to tsx

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Header from './components/Header/Header'
+
+import './App.css'
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <Header />
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  )
+}
+
+function Home() {
+  return <h2>Home</h2>
+}
+
+function About() {
+  return <h2>About</h2>
+}
+
+function Users() {
+  return <h2>Users</h2>
+}
