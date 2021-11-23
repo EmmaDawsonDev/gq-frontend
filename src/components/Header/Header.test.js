8c20bd954b9de0cd770e87687904d2212b1e16fe
Emma Dawson <emma.l.dawson@gmail.com>
2021-11-23 09:16:11 +0100

    Add notes to header tests

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -9,3 +9,10 @@ describe('Header', () => {
     expect(getByText(/geoQuizzr/)).toBeTruthy()
   })
 })
+
+// Header should show login and signup buttons when logged out
+// Header should not show login and signup buttons when loggged in
+// Header should show logout button when signed in
+// Header should not show logout button when signed out
+// Header should be hamburger on small screens
+// Header should not show hamburger on large screens
