6d1b5c1f516966b2f5302863996242818175466d
Emma Dawson <emma.l.dawson@gmail.com>
2021-11-23 11:55:41 +0100

    Added header image and text to landing page

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
 
+import styles from './Landing.module.css'
+
 const Landing = () => {
-  return <div>Landing page</div>
+  return (
+    <main>
+      <div className={styles.headerImg}>
+        <div className={styles.landingHeaderText}>
+          <h1>Explore the world</h1>
+          <h2>one question at a time</h2>
+        </div>
+      </div>
+    </main>
+  )
 }
 
 export default Landing
