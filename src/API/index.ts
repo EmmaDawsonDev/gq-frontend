bac0f0c9ed38eda894d258bc44d3cf849322435a
Emma Dawson <emma.l.dawson@gmail.com>
2022-02-18 15:37:28 +0100

    update API file to typescript

---
+++ src/API/index.ts
@@ -0,0 +1,34 @@
+import axios from 'axios'
+
+const API = axios.create({
+  baseURL: 'https://gq-backend.herokuapp.com/api/v1',
+})
+
+export const setDefaultHeaders = (token: string) => {
+  API.defaults.headers.common['Authorization'] = `Bearer ${token}`
+}
+
+export const login = async ({ email, password }: { email: string; password: string }) => {
+  try {
+    const response = await API.post('/authenticate', {
+      email,
+      password,
+    })
+
+    if (response.status === 200) {
+      setDefaultHeaders(response.data.token)
+      console.log(response.data)
+      return response.data
+    } else {
+      return false
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// login({
+
+//   "email": "pelle@test.com",
+//   "password": "pelle123"
+// })
