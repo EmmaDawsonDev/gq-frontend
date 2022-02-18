d01a5d397471edc6c4d575d1f1a085d805070dbb
Emma Dawson <emma.l.dawson@gmail.com>
2022-02-18 15:31:36 +0100

    Updated center of map

--- src/pages/Play/Play.tsx
+++ src/pages/Play/Play.tsx
@@ -3,16 +3,16 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import styles from './Play.module.css'
 
 const Play = () => {
-  // return <div>Hello</div>
+  
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} className={styles.leafletContainer}>
+    <MapContainer center={[59.3288676, 18.0617572]} zoom={15} className={styles.leafletContainer}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
+      <Marker position={[59.3288676, 18.0617572]}>
         <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
+          geoQuizzr is currently a work in progress <br /> Come back in May 2022 to play
         </Popup>
       </Marker>
     </MapContainer>
