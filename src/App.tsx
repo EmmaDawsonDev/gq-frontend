ee56a81889fb35403bdbf327672260ffd1b224e2
Emma Dawson <emma.l.dawson@gmail.com>
2022-02-18 14:54:05 +0100

    Tweaked styling

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Header from './components/Header/Header'
 // import FAQs from './pages/FAQs/FAQs'
 import Landing from './pages/Landing/Landing'
 import Login from './pages/Login/Login'
-// import Play from './pages/Play/Play'
+import Play from './pages/Play/Play'
 // import Profile from './pages/Profile/Profile'
 import Signup from './pages/Signup/Signup'
 // import NotFound from './pages/NotFound/NotFound'
@@ -14,33 +14,33 @@ import './App.css'
 export default function App() {
   return (
     <Router>
-        <Header />
-        <Switch>
-          {/* <Route path="/FAQs">
+      <Header />
+      <Switch>
+        {/* <Route path="/FAQs">
             <FAQs />
           </Route>
           
-          <Route path="/play">
-            <Play />
-          </Route>
+          
           <Route path="/profile">
             <Profile />
           </Route>
            */}
-          <Route exact path="/">
-            <Landing />
-          </Route>
-          <Route path="/signup">
-            <Signup />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          {/* <Route>
+        <Route exact path="/">
+          <Landing />
+        </Route>
+        <Route path="/signup">
+          <Signup />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/play">
+          <Play />
+        </Route>
+        {/* <Route>
             <NotFound />
           </Route> */}
-        </Switch>
-      
+      </Switch>
     </Router>
   )
 }
