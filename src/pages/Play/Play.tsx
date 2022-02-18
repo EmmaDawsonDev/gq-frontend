1b1bee14a2a34c24d6bcf3f11c5f4481bdb4d89e
Emma Dawson <emma.l.dawson@gmail.com>
2022-02-18 15:47:06 +0100

    Add main tag to map page

--- src/pages/Play/Play.tsx
+++ src/pages/Play/Play.tsx
@@ -5,17 +5,19 @@ import styles from './Play.module.css'
 const Play = () => {
   
   return (
-    <MapContainer center={[59.3288676, 18.0617572]} zoom={15} className={styles.leafletContainer}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[59.3288676, 18.0617572]}>
-        <Popup>
-          geoQuizzr is currently a work in progress <br /> Come back in May 2022 to play
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <main>
+      <MapContainer center={[59.3288676, 18.0617572]} zoom={15} className={styles.leafletContainer}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[59.3288676, 18.0617572]}>
+          <Popup>
+            geoQuizzr is currently a work in progress <br /> Come back in May 2022 to play
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </main>
   )
 }
 
