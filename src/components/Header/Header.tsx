a943c2a7cad2363e32c908453aba5667a4c25d4a
Emma Dawson <emma.l.dawson@gmail.com>
2022-04-04 21:56:24 +0200

    Add link to profile page in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import styles from './Header.module.css'
 import { Link, useHistory } from 'react-router-dom'
 import Logo from '../../assets/logos/gq-logo-white.svg'
+import accountWhite from '../../assets/icons/account-white.png'
 import { useAppSelector } from '../../hooks/useAppSelector'
 import { useAppDispatch } from '../../hooks/useAppDispatch'
 import { logoutUser } from '../../store/user/userSlice.actions'
@@ -18,10 +19,10 @@ const Header = () => {
 
   return (
     <header className={styles.header}>
-      <section className={styles.logoContainer}>
+      <Link to={user ? '/play' : '/'} className={styles.logoContainer}>
         <img src={Logo} alt="" className={styles.logo} />
         <p className={styles.logoText}>geoQuizzr</p>
-      </section>
+      </Link>
       {!user && (
         <div className={styles.buttonContainer}>
           <Link to="/login" className={styles.linkOutline}>
@@ -34,6 +35,9 @@ const Header = () => {
       )}
       {user && (
         <div className={styles.buttonContainer}>
+          <Link to="/profile">
+            <img src={accountWhite} alt="profile icon" className={styles.profileImage} />
+          </Link>
           <button className={styles.buttonOutline} type="button" onClick={handleLogout}>
             Log out
           </button>
